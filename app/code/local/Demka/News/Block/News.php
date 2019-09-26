1c5a29a02d7d5497f22466a66d7180354e261334
Dmytro Kamyshov <dmytro.kamyshov@transoftgroup.com>
2019-09-26 12:35:20 +0300

    refs #59147 #59148 Создать модуль News (added sorter)

--- app/code/local/Demka/News/Block/News.php
+++ app/code/local/Demka/News/Block/News.php
@@ -4,6 +4,7 @@ class Demka_News_Block_News extends Mage_Page_Block_Html_Pager
 {
     public function _construct()
     {
+        /** @var Demka_News_Model_Resource_News_Collection $collection */
         $collection = Mage::getModel('demkanews/news')->getCollection();
         $this->setCollection($collection);
     }
@@ -12,17 +13,31 @@ class Demka_News_Block_News extends Mage_Page_Block_Html_Pager
     {
         parent::_prepareLayout();
 
+        /** @var Mage_Page_Block_Html_Pager $pager */
         $pager = $this->getLayout()->createBlock('page/html_pager', 'custom.pager');
         $pager->setAvailableLimit(array(2 => 2, 10 => 10, 20 => 20, 'all' => 'all'));
         $pager->setCollection($this->getCollection());
         $this->setChild('pager', $pager);
-        $this->getCollection()->load();
+
+        /** @var Demka_News_Block_Sorter $sorter */
+        $sorter = $this->getLayout()->createBlock('demkanews/sorter', 'custom.sorter');
+        $this->setChild('sorter', $sorter);
+
+        $dir = $sorter->getCurrentDirection();
+        $orderBy = $sorter->getCurrentOrder();
+
+        $this->getCollection()->addOrder($orderBy, $dir)->load();
         return $this;
     }
 
     public function getPagerHtml()
     {
         return $this->getChildHtml('pager');
+    }
+
+    public function getSorterHtml()
+    {
+        return $this->getChildHtml('sorter');
     }
 
     public function getNewsCollection()
@@ -33,5 +48,10 @@ class Demka_News_Block_News extends Mage_Page_Block_Html_Pager
     public function truncateNewsItem($text, $length = 100)
     {
         return mb_substr($this->stripTags($text), 0, $length);
+    }
+
+    public function formatDate($date = null, $format = Mage_Core_Model_Locale::FORMAT_TYPE_SHORT, $showTime = false)
+    {
+        return parent::formatDate($date, $format, $showTime);
     }
 }
